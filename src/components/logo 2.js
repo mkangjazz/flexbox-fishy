8d295836d57fe248d8ef38d1f840b3c08f334099
Mike Kang Jazz <kaizenmike@gmail.com>
2021-03-21 15:23:42 -0500

    graphic and logo and colors

---
+++ src/components/logo 2.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Logo() {
+  return (
+    <h1 className='logo'>
+      <a href='/'>
+        <svg xmlns="http://www.w3.org/2000/svg" width="2423" height="295" viewBox="0 0 2423 295">
+  <text id="FL_XBOX_FISHY" data-name="FL  XBOX FISHY" transform="translate(0 236)" fill="#ff7f00" font-size="295" font-family="GothamRounded-Medium, Gotham Rounded" font-weight="500"><tspan x="0" y="0" xml:space="preserve">FL  XBOX FISHY</tspan></text>
+</svg>
+
+      </a>
+      <span className='sr-only'>
+        Flexbox Fishy
+      </span>
+    </h1>
+  );
+}
