64c1b983a523f16650564536ab75935b3a52c46c
Mike Kang Jazz <kaizenmike@gmail.com>
2023-02-19 10:06:52 -0600

    Updates

---
+++ precache-manifest.2598c5edb3bab3b8e79be01ef4de6f35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a255b9f7f7b0f2b7a283e6677668693",
+    "url": "/flexbox-fishy/index.html"
+  },
+  {
+    "revision": "8aadaeb5780bf7e09983",
+    "url": "/flexbox-fishy/static/css/main.35f80d9c.chunk.css"
+  },
+  {
+    "revision": "2004c1bb5180f786c996",
+    "url": "/flexbox-fishy/static/js/2.6f6bf316.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flexbox-fishy/static/js/2.6f6bf316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aadaeb5780bf7e09983",
+    "url": "/flexbox-fishy/static/js/main.be71f852.chunk.js"
+  },
+  {
+    "revision": "dd6a0e34cae4f83ccf7e",
+    "url": "/flexbox-fishy/static/js/runtime-main.9fb39ba8.js"
+  }
+]);
